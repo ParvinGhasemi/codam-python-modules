d602b3675c1f3893cafbd12a2862f77634354a08
parvinghasemi <mghasemi.parvin@gmail.com>
2026-06-08 10:06:35 +0200

    adding setters & getters to plant class

--- module_01/ex6/ft_garden_analytics.py
+++ module_01/ex6/ft_garden_analytics.py
@@ -10,8 +10,25 @@ class Plant:
         self.set_height(height)
         self.set_age(age)
 
-    def set_height(self, height) -> None:
+    def set_height(self, height: float) -> bool:
+        if height < 0:
+            print(
+                f"{self._name.capitalize()}: "
+                f"Error, height can't be negative"
+            )
+            return False
         self._height = height
+        return True
 
-    def set_age(self, age) -> None:
+    def set_age(self, age: int) -> bool:
+        if age < 0:
+            print(f"{self._name.capitalize()}: Error, age can't be negative")
+            return False
         self._age = age
+        return True
+
+    def get_height(self) -> float:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age
